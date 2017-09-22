06844472839dd9f4fe3123052c306c07f749ec05
Edouard Menayde <scrunshes@gmail.com>
2017-09-22 15:46:23 +0200

    Setup service worker

---
+++ sw-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  staticFileGlobs: [
+    'public/css/**.css',
+    'public/**/**/**/**/**.html',
+    'public/images/**.jpg',
+    'public/images/**.png',
+    'public/js/**.js',
+    'public/lib/**/css/**.css',
+    'public/lib/**/**/**.otf',
+    'public/lib/**/**/**.svg',
+    'public/lib/**/**/**.ttf',
+    'public/lib/**/**/**.woff',
+    'public/lib/**/**/**.woff2',
+    'public/lib/**/**.js',
+    'public/lib/**/**.js',
+  ],
+  stripPrefix    : 'public',
+};
